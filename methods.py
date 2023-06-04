f0c4062ecbec8660fad1a55fcb138e08b7ba9a1b
Varshith <gvarshithreddy8@gmail.com>
2023-06-04 16:28:22 +0530

    Added new methods
    1. Add many students data through a csv file. It takes year,branch,section and csv file as input

---
+++ methods.py
@@ -0,0 +1,58 @@
+import csv
+from pymongo import MongoClient
+
+def add_students_many(year, branch, section, csv_file):
+    # Connect to MongoDB Atlas cluster
+    # Replace <cluster_uri> with your actual MongoDB Atlas connection string
+    cluster_uri = "mongodb+srv://gvarshithreddy8:Varshith1@cluster0.xzgxe3m.mongodb.net/?retryWrites=true&w=majority"
+    cluster = MongoClient(cluster_uri)
+
+    # Access the SEM database with the correct casing
+    db = cluster.sem  # Replace "SEM" with the correct casing of the existing database
+
+    # Access the studentyear collection
+    studentyear = db.studentyear
+
+    # Find the matching section in the studentyear collection
+    year_document = studentyear.find_one({'_id': year})
+    if year_document is None:
+        print(f"Year {year} does not exist in the database.")
+        cluster.close()
+        return
+    
+    branch_documents = year_document['branches']
+    matching_branch = next((b for b in branch_documents if b['branch'] == branch), None)
+    if matching_branch is None:
+        print(f"Branch {branch} does not exist in Year {year}.")
+        cluster.close()
+        return
+    
+    section_documents = matching_branch['sections']
+    matching_section = next((s for s in section_documents if s['section'] == section), None)
+    if matching_section is None:
+        print(f"Section {section} does not exist in Branch {branch} of Year {year}.")
+        cluster.close()
+        return
+
+    # Parse the CSV file and append studentdata documents to the section
+    studentdata_collection = db.studentdata
+    
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row
+        
+        for row in reader:
+            rollno, studentname = row
+            student = {
+                'rollno': rollno,
+                'studentname': studentname
+            }
+            
+            # Append the student document to the respective section
+            matching_section.setdefault('students', []).append(student)
+    
+    studentyear.replace_one({'_id': year}, year_document)
+    print("Students added successfully.")
+
+    # Close the MongoDB connection
+    cluster.close()
