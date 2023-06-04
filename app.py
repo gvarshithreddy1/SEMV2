aabc31f32fe503c2c8d5ef6a8c082bccd419a84d
Varshith <gvarshithreddy8@gmail.com>
2023-06-04 15:52:19 +0530

    added base required html pages and removed mongo object created since its not needed

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONOGO_URI"] = "mongodb+srv://gvarshithreddy8:<password>@cluster0.xzgxe3m.mongodb.net/?retryWrites=true&w=majority"
-mongo = PyMongo(app)
-@app.route("/")
+#mongo = PyMongo(app)
+
+@app.route("/home")
 def home():
     return render_template("home.html")
 
